055e797ac0e4b5e7c6ef9dd70c014016fc1f23d9
yogeshsnous <Yogesh.S@365smartshop.com>
2024-06-21 11:03:16 +0530

    api integration

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -11,58 +11,64 @@ type detailsProps = {
     
 }
 
-
+type user = {
+    id: number,
+    email: string,
+    first_name: string,
+    last_name: string,
+    avatar: string,
+}
 
 
 const DetailsScreen = (props: detailsProps) => {
 
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [users, setUser] = useState([])
 
-    const data = [
-        {day: "Monday", data: ["50Rs", "120Rs", "30Rs", "100Rs"]},
-
-        {day: "Wednesday", data: ["150Rs", "220Rs", "20Rs"]},
-        {day: "Tuesday", data: ["30Rs", "40Rs", "50Rs"]},
-    ]
+    const data: any = {
+        firstName: "Michael",
+        lastName: "Jphn"
+    }
 
     useEffect(() => {
-        fetchAPI()
+        fetchUserList()
+
     }, [])
 
-    const fetchAPI =  async () => {
+    const fetchUserList = async () => {
+        setLoading(true);
+    
 
+
+        try{
+            const res = await fetch("https://reqres.in/api/users?page=2", {method: "GET"})
+            const responseJson = await res.json();
+            setUser(responseJson.data)
+
+            
+
+        } catch {
+           
+
+        } finally {
+            setLoading(false)
+        }
        
-
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(
-
-      )
-      .then(json => console.log(json))
-
-      console.log()
        
     }
 
 
 
+
     return(
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        {/* <SectionList 
-        sections={data}
-        keyExtractor={(item, index) => item+index}
-        renderSectionHeader={({section: {day}}) => (
-            <Text style={{height: 50, width: 200, fontSize: 22 }}>{day}</Text>
-        )}
-        renderItem={({item}) => (
-            <Text style={{height: 50, width: 200, fontSize: 15 }}>{item}</Text>
-        )}
-        /> */}
+       
 
-        {loading ? <ActivityIndicator size={"large"} color={"red"}/> : <Text>Loaded Data</Text>}
-
-        <View style={{marginTop: 50}}>
-            <Button title='Done' onPress={() => {setLoading(!loading)}} />
-        </View>
+        {loading ? <ActivityIndicator size={"large"} color={"red"}/> : 
+        <FlatList 
+        data={users}
+        renderItem={({item}) => <Text style={{fontSize: 14, margin: 10}}>{item.first_name}</Text>}
+        />}
         
         
     </View>
